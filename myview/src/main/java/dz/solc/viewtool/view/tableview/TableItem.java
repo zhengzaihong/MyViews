1e5a5de139f072809fffa0d32e290e550965b0bb
zhengzaihong <1096877329@qq.com>
2019-12-09 15:43:31 +0800

    0.1.92版本更新 表格新特性

--- myview/src/main/java/dz/solc/viewtool/view/tableview/TableItem.java
+++ myview/src/main/java/dz/solc/viewtool/view/tableview/TableItem.java
@@ -3,7 +3,6 @@ package dz.solc.viewtool.view.tableview;
 import android.content.Context;
 import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
