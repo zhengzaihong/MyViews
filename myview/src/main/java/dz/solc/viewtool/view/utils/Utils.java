75d6c9c59ff9780945a6ad720ac7558b77a807ce
zhengzaihong <1096877329@qq.com>
2019-12-16 18:58:41 +0800

    0.1.93版本更新新增加特性和控件

--- myview/src/main/java/dz/solc/viewtool/view/utils/Utils.java
+++ myview/src/main/java/dz/solc/viewtool/view/utils/Utils.java
@@ -7,44 +7,42 @@ import android.view.WindowManager;
 
 public class Utils {
 
-  public static int dp2px(Context context, int dpVal) {
-    final float scale = context.getResources().getDisplayMetrics().density;
-    return (int) (dpVal * scale + 0.5f);
-  }
+    public static int dp2px(Context context, int dpVal) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpVal * scale + 0.5f);
+    }
 
-  public static int sp2px(Context context, int spVal) {
-    final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
-    return (int) (spVal * fontScale + 0.5f);
-  }
+    public static int sp2px(Context context, int spVal) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spVal * fontScale + 0.5f);
+    }
 
-  /**
-   * 获得屏幕高度
-   *
-   * @param context
-   * @return
-   */
-  public static int getScreenWidth(Context context)
-  {
-    WindowManager wm = (WindowManager) context
-            .getSystemService(Context.WINDOW_SERVICE);
-    DisplayMetrics outMetrics = new DisplayMetrics();
-    wm.getDefaultDisplay().getMetrics(outMetrics);
-    return outMetrics.widthPixels;
-  }
+    /**
+     * 获得屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
 
-  /**
-   * 获得屏幕宽度
-   *
-   * @param context
-   * @return
-   */
-  public static int getScreenHeight(Context context)
-  {
-    WindowManager wm = (WindowManager) context
-            .getSystemService(Context.WINDOW_SERVICE);
-    DisplayMetrics outMetrics = new DisplayMetrics();
-    wm.getDefaultDisplay().getMetrics(outMetrics);
-    return outMetrics.heightPixels;
-  }
+    /**
+     * 获得屏幕宽度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
 
 }
